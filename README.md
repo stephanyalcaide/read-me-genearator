0962549157ea33a094a8364068c2c39938aa4d90
Stephany Alcaide <81532183+stephanyalcaide@users.noreply.github.com>
2021-11-16 13:59:33 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# read-me-genearator
+# read-generator
+    
+#### Table of Contents
+1. [Project escription](#project-description)
+2. [Installation Instructions](#installation-instructions)
+3. [Usage Information](#usage-information)
+4. [Contributor Guidelines](#contributor-guidelines)
+5. [Code of conduct](#code-of-conduct)
+6. [Test Instructions](#test-instructions)
+7. [License](#license)
+8. [Questions](#questions)
+
+
+## Project Description
+* makes professional readmes
+
+## Installation Instructions
+* yes inquirer if you don't have it 
+
+## Usage Information
+* to create professional readme's
+
+## Contributor Guidelines
+* all listed in the code of conduct
+
+## Code of Conduct
+* [Contributor Covenant Code of Conduct](https://www.contributor-covenant.org/version/2/0/code_of_conduct/code_of_conduct.md)
+
+## Test Instructions
+* none
+
+## License
+* licensed under the [MIT License](LICENSE.txt)
+
+## Questions
+* For additional help or questions about collaboration, please reach out to stephany05alde@gmail.com
+
+* Follow me on Github at [stephanyalcaide](http://github.com/stephanyalcaide)
---
+++ index.js
@@ -0,0 +1,115 @@
+let inquirer = require("inquirer");
+let fs = require("fs");
+let util = require("util");
+let writeFileAsync = util.promisify(fs.writeFile);
+
+function promptUser() {
+    
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "project_title",
+            message: "What is your project tittled?"
+          },
+          {
+            type: "input",
+            name: "description",
+            message: "Briefly describe your project"
+          },
+          {
+            type: "input",
+            name: "install",
+            message: "Are there any installations required?"
+          },
+          {
+            type: "input",
+            name: "use",
+            message: "What is the use of the application"
+          },
+          {
+            type: "input",
+            name: "contributions",
+            message: "Are there any contribution rules?"
+          },
+          {
+            type: "input",
+            name: "test",
+            message: "Please provide test instructions if applicable"
+          },
+          {
+            type: "checkbox",
+            message: "License?",
+            name: "license",
+            choices: [
+              "[MIT License](LICENSE.txt)", 
+              "[GNU GPLv3 License](COPYING.txt)", 
+            ]
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "Enter your email account"
+          },
+          {
+            type: "input",
+            name: "github",
+            message: "Enter your github username"
+          }
+        ]);
+      }
+
+function generateREADME(answers) {
+  return `# ${answers.project_title}
+    
+#### Table of Contents
+1. [Project escription](#project-description)
+2. [Installation Instructions](#installation-instructions)
+3. [Usage Information](#usage-information)
+4. [Contributor Guidelines](#contributor-guidelines)
+5. [Code of conduct](#code-of-conduct)
+6. [Test Instructions](#test-instructions)
+7. [License](#license)
+8. [Questions](#questions)
+
+
+## Project Description
+* ${answers.description}
+
+## Installation Instructions
+* ${answers.install}
+
+## Usage Information
+* ${answers.use}
+
+## Contributor Guidelines
+* ${answers.contributions}
+
+## Code of Conduct
+* [Contributor Covenant Code of Conduct](https://www.contributor-covenant.org/version/2/0/code_of_conduct/code_of_conduct.md)
+
+## Test Instructions
+* ${answers.test}
+
+## License
+* licensed under the ${answers.license}
+
+## Questions
+* For additional help or questions about collaboration, please reach out to ${answers.email}
+
+* Follow me on Github at [${answers.github}](http://github.com/${answers.github})`;
+  
+}
+
+promptUser()
+  .then(function(answers) {
+    const readme = generateREADME(answers);
+
+ 
+    return writeFileAsync("README.md", readme);
+  })
+  .then(function() {
+    console.log(" README.md has been created!");
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
